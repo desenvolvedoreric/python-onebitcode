bfd781a856a1ba03df6f9ed29941b4c0ba29c915
desenvolvedoreric <desenvolvedoreric@gmail.com>
2024-05-20 23:52:08 -0300

    feat: [Fundamentos] input em python.

---
+++ 1-fundamentos-python/3-aula/input.py
@@ -0,0 +1,9 @@
+name = input("Qual o nome do jogo?\n")
+yearLaunch = int(input("Qual o ano do jogo?\n"))
+isExpensive = input("É um jogo caro?\n")
+price = float(input("Qual o valor do jogo?\n"))
+
+print(name)
+print(yearLaunch)
+print(isExpensive)
+print(price)
