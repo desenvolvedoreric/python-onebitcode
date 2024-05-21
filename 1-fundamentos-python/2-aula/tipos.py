7af4dabf45e786f3f5fed70d566c872ed75081ad
desenvolvedoreric <desenvolvedoreric@gmail.com>
2024-05-20 23:34:49 -0300

    feat: [Fundamentos] aula 2 - variaveis e tipos

---
+++ 1-fundamentos-python/2-aula/tipos.py
@@ -0,0 +1,14 @@
+name = "Killzone - Mercenary"
+yearLaunch = 2013
+isGame = True
+price = 5.00
+
+print(name)
+print(yearLaunch)
+print(isGame)
+print(price)
+
+print(type(name))
+print(type(yearLaunch))
+print(type(isGame))
+print(type(price))
