b9bbd0fc1c353fd00d67f49f8b4977307880104e
desenvolvedoreric <desenvolvedoreric@gmail.com>
2024-05-23 00:05:34 -0300

    feat: [Fundamentos] slice string

---
+++ 1-fundamentos-python/8-aula/slice-string.py
@@ -0,0 +1,16 @@
+name = "Eric Oliveira"
+
+# Get all string from first position.
+print(name[0:])
+
+# Get all string from index 2.
+print(name[2:])
+
+# Remove the last char of string.
+print(name[:-1])
+
+# Get string step two positions.
+print(name[::2])
+
+# Reverse string.
+print(name[::-1])
