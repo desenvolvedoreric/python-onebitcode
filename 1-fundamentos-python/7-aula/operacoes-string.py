160474a0867a2847a9efc8605aaf6b3afbee20d2
desenvolvedoreric <desenvolvedoreric@gmail.com>
2024-05-22 23:41:34 -0300

    feat: [Fundamentos] Operações com string.

---
+++ 1-fundamentos-python/7-aula/operacoes-string.py
@@ -0,0 +1,18 @@
+name = "Filhos da anarquia"
+description = """
+Uma gangue de motoqueiros segue suas próprias leis e comanda, 
+por debaixo dos panos, o tráfico de armas na região da aparentemente pacata 
+da cidade de Charming, protegendo-a contra forasteiros hostis.
+"""
+season = "Temporada"
+seasonNumber = "1"
+
+# concatenate String
+fullSeason = season + " " + seasonNumber
+print(fullSeason)
+
+# Repeat String
+print("=" * 20)
+
+# Includes word in this string?
+print("tráfico" in description)
