1f7635d20d3bf52626faf8a290da6ab23270ec75
desenvolvedoreric <desenvolvedoreric@gmail.com>
2024-05-23 23:58:58 -0300

    feat: [Fundamento] Métodos de string.

---
+++ 1-fundamentos-python/9-aula/metodos-string.py
@@ -0,0 +1,28 @@
+name = "Prince Of Persia"
+
+# Deixa em maiuscula. 
+print(name.upper()) 
+
+# Deixa em minuscula.
+print(name.lower())  
+
+# Deixa primeira letra de cada palavra maiuscula.
+print(name.capitalize())  
+
+# Deixa primeira letra maiuscula. 
+print(name.title()) 
+
+# Centraliza palavra
+print(name.center(20, "-"))  
+
+# Retorna posição do caracter encontrado. 
+print(name.find("a")) 
+
+# Conta quantos caracteres contém na string. 
+print(name.count("a")) 
+
+# Troca um determinado valor da string
+print(name.replace("Persia", "World")) 
+
+# Quebra a string e transforma em uma lista.
+print(name.split(" ")) 
