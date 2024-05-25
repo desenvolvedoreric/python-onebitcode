fa0ee1a21edaefd420e7c71d049477669f5e651a
desenvolvedoreric <desenvolvedoreric@gmail.com>
2024-05-24 23:19:32 -0300

    feat: [Fundamentos] Listas, aula 10

---
+++ 1-fundamentos-python/10-aula/listas.py
@@ -0,0 +1,10 @@
+movies = ["Harry Potter", "Velozes e Furiosos", "Sempre Ao Seu Lado", "Star Wars"]
+
+# Acessando a posição de um item.
+print(movies[0])
+
+# Acessando da posição 1 em diante.
+print(movies[1:])
+
+# Acessando o último elemento.
+print(movies[-1])
