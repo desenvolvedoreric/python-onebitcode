0404ca096c4fc3fd64bf7c8ba6ced01614bffd4c
desenvolvedoreric <desenvolvedoreric@gmail.com>
2024-05-26 14:59:37 -0300

    feat: [Fundamentos] tuplas.

---
+++ 1-fundamentos-python/12-aula/tuplas.py
@@ -0,0 +1,15 @@
+movies = ("Harry Potter", "Velozes e Furiosos", "Sempre Ao Seu Lado", "Star Wars")
+
+# Buscar o primeiro elemento
+print(movies[:1])
+
+# Buscar o último elemento
+print(movies[-1])
+
+# Buscar de uma posição especifica
+print(movies[2:])
+
+# Buscar indice do elemento
+print(movies.index("Velozes e Furiosos"))
+
+# Observação: Não pode adicionar ou remover elementos e ordernar valores.
