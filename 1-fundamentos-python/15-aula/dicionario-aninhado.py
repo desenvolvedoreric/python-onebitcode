2133cfe27ba8c7a23fdb3a5693a799951ee7240b
desenvolvedoreric <desenvolvedoreric@gmail.com>
2024-05-26 15:56:22 -0300

    feat: [Fundamentos] Dicionario aninhado.

---
+++ 1-fundamentos-python/15-aula/dicionario-aninhado.py
@@ -0,0 +1,13 @@
+movies = {
+  "Harry Potter": {
+    "author": "J. K. Rowling",
+    "year": "1998",
+  },
+  "Velozes e Furiosos": {
+    "author": "Rob Cohen",
+    "year": "2001"
+  }
+}
+
+# Pegar um valor especifico.
+print(movies["Harry Potter"]["author"])
