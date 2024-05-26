2d2093ebfc6a143e7c363bc628dbff2c90287ca7
desenvolvedoreric <desenvolvedoreric@gmail.com>
2024-05-26 15:17:24 -0300

    feat: [Fundamentos] Set.

---
+++ 1-fundamentos-python/13-aula/set.py
@@ -0,0 +1,15 @@
+movies = {"Harry Potter", "Velozes e Furiosos"}
+
+# Adicionar um elemento
+movies.add("Sempre Ao Seu Lado")
+print(movies)
+
+# Remover um elemento
+movies.remove("Sempre Ao Seu Lado")
+print(movies)
+
+# Atualizar mais de um elemento 
+movies.update({"Star Wars", "Interestelar"})
+print(movies)
+
+# Observação: Não podemos adicionar valores iguais. 
